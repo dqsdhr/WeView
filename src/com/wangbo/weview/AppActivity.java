b4de9b8b117262016208cc36f8bc9937817bfdc6
Mark <dqsdhr@gmail.com>
2014-04-29 15:19:42 +0800

    first

---
+++ src/com/wangbo/weview/AppActivity.java
@@ -0,0 +1,87 @@
+package com.wangbo.weview;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.app.Fragment;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class AppActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// 加载layout
+		setContentView(R.layout.activity_app);
+
+		if (savedInstanceState == null) {
+			getFragmentManager().beginTransaction().add(R.id.container, new VideoFragment()).commit();
+		}
+	}
+
+	/**
+	 * 在运行状态保存需要夸activity和fragment的基础数据
+	 * 
+	 * @param savedInstanceState
+	 */
+	public void onSavedInstanceState(Bundle savedInstanceState) {
+	}
+
+	public void findView() {
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.app, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	@SuppressLint("ValidFragment")
+	public static class VideoFragment extends Fragment {
+
+		public VideoFragment() {
+
+		}
+
+		MediaController mediaController;
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_app, container, false);
+			VideoView videoView_cemara1 = (VideoView) rootView.findViewById(R.id.videoView_cemara1);
+
+			// 创建MediaController对象
+			// mediaController = new MediaController(getActivity());
+			String cemaraOnePath = this.getString(R.string.cemara1_path);
+			Uri cemaraOneUri = Uri.parse(cemaraOnePath);
+			videoView_cemara1.setVideoURI(cemaraOneUri);
+			// videoView_cemara1.setMediaController(mediaController);
+			// mediaController.setMediaPlayer(videoView_cemara1);
+			videoView_cemara1.requestFocus();
+			videoView_cemara1.start();
+			videoView_cemara1.stopPlayback();
+			videoView_cemara1.start();
+			return rootView;
+		}
+	}
+
+}
---
+++ src/com/wangbo/weview/MainActivity.java
@@ -0,0 +1,53 @@
+package com.wangbo.weview;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			new Handler().postDelayed(new Runnable() {
+
+				@Override
+				public void run() {
+					Intent intent = new Intent(MainActivity.this, AppActivity.class);
+					startActivity(intent);
+					MainActivity.this.finish();
+					overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+				}
+			}, 3000);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+
+
+}
